21ccda89c938b5b1a9fab95cfece3d659e0c0678
swastikpradhan056 <swastikpradhan056@gmail.com>
2025-01-01 22:33:02 +0545

    Packages Change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
